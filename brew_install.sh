8f6409d9a6a17cb337dd1ea0044748dfdf01a2b8
Daniil Migachev <dan@Daniils-MacBook-Pro.local>
2020-08-18 14:16:57 +0300

    Added powerlevel10k support

--- brew_install.sh
+++ brew_install.sh
@@ -5,11 +5,16 @@ brew update
 brew upgrade
 
 brew install htop
+brew install tree
+brew install pstree
 
 # set up python
 brew install python3
 ln -s -f /usr/local/bin/python3 /usr/local/bin/python
 ln -s -f /usr/local/bin/pip3 /usr/local/bin/pip
+
+# networking
+brew install httpie
 
 # misc
 brew install terminal-notifier
--- install.sh
+++ install.sh
@@ -13,6 +13,18 @@ if [[ ! -f "$HOME/.scm_breeze/scm_breeze.sh" ]]; then
 	~/.scm_breeze/install.sh
 fi;
 
+# Install p10k
+# https://github.com/romkatv/powerlevel10k
+# https://qiita.com/szk07/items/b15c38ec73e547a23439
+if [[ -f ~/.p10k.zsh ]]; then
+    # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+    source ~/.p10k.zsh
+else
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    source ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k/powerlevel10k.zsh-theme	
+fi;
+
+
 # install brew packages, replace with Brewfile later
 . ${BASEDIR}/brew_install.sh
 
